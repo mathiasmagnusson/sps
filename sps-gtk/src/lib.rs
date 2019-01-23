b48b9805013593beebd1a01588c64b43b2ffc546
mathiasmagnusson <mathiasmagnussons@gmail.com>
2019-01-23 17:09:24 +0100

    Make shit work

--- sps-gtk/src/lib.rs
+++ sps-gtk/src/lib.rs
@@ -1,36 +1,115 @@
-#[derive(Clone, Debug)]
+use std::{collections::HashMap, u32};
+
+#[derive(Clone, Debug, Copy)]
 pub struct Position {
-	row: u32,
-	col: u32,
+	pub col: u32,
+	pub row: u32,
 }
+
+pub type ItemId = u32; // perhaps a string, this can be the barcode
 
 #[derive(Clone, Debug)]
 pub struct Item {
 	name: String,
-	barcode: String, // perhaps just a number or something
-	pos: Position,
+	id: ItemId,
 }
 
+impl Item {
+	pub fn new(name: &str, id: ItemId) -> Self {
+		Self {
+			name: name.into(),
+			id,
+		}
+	}
+	pub fn id(&self) -> ItemId {
+		self.id
+	}
+}
+
+pub struct Store {
+	item_map: HashMap<ItemId, Position>,
+	entrance: Position,
+	checkout: Position,
+}
+
+impl Store {
+	pub fn new(
+		item_map: HashMap<ItemId, Position>,
+		entrance: Position,
+		checkout: Position,
+	) -> Self {
+		Self {
+			item_map,
+			entrance,
+			checkout,
+		}
+	}
+	fn get_pos_of_item(&self, id: ItemId) -> Option<Position> {
+		if let Some(position) = self.item_map.get(&id) {
+			Some(*position)
+		} else {
+			None
+		}
+	}
+}
+
+#[derive(Debug)]
 pub struct Shoplist {
 	items: Vec<Item>,
+	walking_length: Option<u32>,
 }
 
 impl Shoplist {
-	pub fn reorder_path(&mut self, _entrance: Position, _checkout: Position) {
+	pub fn new(items: Vec<Item>) -> Self {
+		Self { items, walking_length: None }
+	}
+	pub fn reorder_path(&mut self, store: Store) {
 		let mut perms = Vec::new();
 
 		permutations(vec![], self.items.clone(), &mut perms);
 
-		for _perm in perms {
-			// get length of perm
-			// remember to start at entrance, not the first item in perm and end at checkout
-			// save which perm is shortest
+		let mut shortest_perm = (vec![], u32::MAX);
+		for perm in perms {
+			let length: u32 = {
+				let item = store
+					.get_pos_of_item(perm[0].id())
+					.expect("This store misses some item in your shopping list");
+
+				((store.entrance.col as i64 - item.col as i64).abs()
+					+ (store.entrance.row as i64 - item.row as i64).abs()) as u32
+			} + perm
+				.iter()
+				.zip(perm.iter().skip(1))
+				.map(|(item1, item2)| {
+					let pos1 = store
+						.get_pos_of_item(item1.id())
+						.expect("This store misses some item in your shopping list");
+					let pos2 = store
+						.get_pos_of_item(item2.id())
+						.expect("This store misses some item in your shopping list");
+
+					((pos1.col as i64 - pos2.col as i64).abs()
+						+ (pos1.row as i64 - pos2.row as i64).abs()) as u32
+				})
+				.sum::<u32>()
+				+ {
+					let item = store
+						.get_pos_of_item(perm[perm.len() - 1].id())
+						.expect("This store misses some item in your shopping list");
+
+					((store.checkout.col as i64 - item.col as i64).abs()
+						+ (store.checkout.row as i64 - item.row as i64).abs()) as u32
+				};
+			if length < shortest_perm.1 {
+				shortest_perm = (perm, length);
+			}
 		}
-		// set reorder self.items to the order in perms
+		self.items = shortest_perm.0;
+		self.walking_length = Some(shortest_perm.1);
 	}
 }
 
-pub fn permutations(select: Vec<Item>, remaining: Vec<Item>, perms: &mut Vec<Vec<Item>>) {
+fn permutations(select: Vec<Item>, remaining: Vec<Item>, perms: &mut Vec<Vec<Item>>) {
 	if remaining.len() == 0 {
 		perms.push(select);
 	} else {
--- sps-gtk/src/main.rs
+++ sps-gtk/src/main.rs
@@ -1,5 +1,25 @@
 mod lib;
 
+use self::lib::*;
+use std::collections::HashMap;
+
 fn main() {
-	
+	let mut shoplist = Shoplist::new(
+		vec![
+			Item::new("Vodka", 0),
+			Item::new("Kvass", 1),
+			Item::new("Sputnik", 2),
+			Item::new("Blini", 3),
+			Item::new("AK-47", 4),
+		]
+	);
+	let mut item_map: HashMap<ItemId, Position> = Default::default();
+	item_map.insert(0, Position { col: 6, row: 1 });
+	item_map.insert(1, Position { col: 0, row: 2 });
+	item_map.insert(2, Position { col: 3, row: 5 });
+	item_map.insert(3, Position { col: 2, row: 2 });
+	item_map.insert(4, Position { col: 3, row: 0 });
+	let store = Store::new(item_map, Position { col: 4, row: 0 }, Position { col: 0, row: 0 });
+	shoplist.reorder_path(store);
+	println!("{:?}", shoplist);
 }
