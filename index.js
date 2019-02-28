83c0dee260a94196fe24508f7af5737506613571
mathiasmagnusson <mathiasmagnussons@gmail.com>
2019-02-28 19:28:40 +0100

    Formatting

--- index.js
+++ index.js
@@ -8,59 +8,27 @@ class Item {
 class Store {
 	constructor() {
 		this.verts = [
-			{
-				items: [new Item("Absolut Vodka", "8850749311446")],
-			},
-			{
-				items: [],
-			},
-			{
-				items: [new Item("Mjölk", "7780721820430"), new Item("Fil", "640509040147")],
-			},
-			{
-				items: [],
-			}
+			{ items: [new Item("Absolut Vodka", "8850749311446")], },
+			{ items: [], },
+			{ items: [new Item("Mjölk", "7780721820430"), new Item("Fil", "640509040147")], },
+			{ items: [], }
 		];
 		this.edges = [
 			[
-				{
-					weight: 3,
-					dest: 1,
-				},
+				{ weight: 3, dest: 1, },
 			],
 			[
-				{
-					weight: 3,
-					dest: 0,
-				},
-				{
-					weight: 4,
-					dest: 2,
-				},
-				{
-					weight: 5,
-					dest: 3,
-				},
+				{ weight: 3, dest: 0, },
+				{ weight: 4, dest: 2, },
+				{ weight: 5, dest: 3, },
 			],
 			[
-				{
-					weight: 4,
-					dest: 1
-				},
-				{
-					weight: 2,
-					dest: 3,
-				}
+				{ weight: 4, dest: 1 },
+				{ weight: 2, dest: 3, }
 			],
 			[
-				{
-					weight: 5,
-					dest: 1,
-				},
-				{
-					weight: 2,
-					dest: 2,
-				}
+				{ weight: 5, dest: 1, },
+				{ weight: 2, dest: 2, }
 			]
 		];
 	}
