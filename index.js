c4d66ae3ef96dd2b70dd6b6a30597bc06e9a602e
mathiasmagnusson <mathiasmagnussons@gmail.com>
2019-02-28 20:53:47 +0100

    Implemented djikstras algorithm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Item {
-	constructor(name, id) {
+	constructor(name, barcode) {
 		this.name = name;
-		this.id = id;
+		this.barcode = barcode;
 	}
 }
 
@@ -19,20 +19,68 @@ class Store {
 			],
 			[
 				{ weight: 3, dest: 0, },
-				{ weight: 4, dest: 2, },
-				{ weight: 5, dest: 3, },
+				{ weight: 7, dest: 2, },
+				{ weight: 1, dest: 3, },
 			],
 			[
-				{ weight: 4, dest: 1 },
+				{ weight: 7, dest: 1 },
 				{ weight: 2, dest: 3, }
 			],
 			[
-				{ weight: 5, dest: 1, },
+				{ weight: 1, dest: 1, },
 				{ weight: 2, dest: 2, }
 			]
 		];
 	}
-	path_between_items(id1, id2)
+	find_item(barcode) {
+		for (let [i, v] of Object.entries(this.verts)) {
+			for (let item of v.items) {
+				if (item.barcode == barcode) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+	path_between_items(barcode1, barcode2) {
+		let src = this.find_item(barcode1);
+		let dst = this.find_item(barcode2);
+
+		let dist = this.verts.map(() => Infinity);
+		let prev = this.verts.map(() => -1);
+		let nx = this.verts.map((_, i) => i);
+
+		dist[src] = 0;
+
+		while (nx.length > 0) {
+			let u = dist.indexOf(Math.min(...nx.map(v => dist[v])));
+
+			nx.splice(nx.indexOf(u), 1);
+
+			if (u == dst) {
+				let path = [];
+
+				while (u != -1) {
+					path.unshift(u);
+					u = prev[u];
+				}
+
+				return { path, length: dist[dst] };
+			}
+
+			for (let { dest: v, weight } of this.edges[u]) {
+				let alt = dist[u] + weight;
+				if (alt < dist[v]) {
+					dist[v] = alt;
+					prev[v] = u;
+				}
+			}
+		}
+
+		console.error("rip in peperonis, this is awful");
+	}
 }
 
 let shoplist = [];
+
+let store = new Store();
